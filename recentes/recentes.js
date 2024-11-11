0fff713cef241faf235a2ede58cc41fd86d5dce4
Kuronosu22 <guitandrade642@gmail.com>
2024-11-11 17:30:31 -0300

    kuro

--- recentes/recentes.js
+++ recentes/recentes.js
@@ -86,3 +86,12 @@ function disableButton(){
 }
 
 document.addEventListener('DOMContentLoaded', fetchRecentNews);
+
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+      x.className += " responsive";
+    } else {
+      x.className = "menu";
+    }
+  }
